1d5e88fd18970efdbd1cfec60ef3d55f8dd627c2
fg2it <fgpg2it@gmail.com>
2017-03-02 00:44:21 +0000

    docker : v4.2.0-beta1

--- docker/v4.x/build.sh
+++ docker/v4.x/build.sh
@@ -1,28 +1,98 @@
 #!/bin/bash
-_repo_tag=$1
-_grafana_stamp=$2
-_docker_tag=$3
-_pkg_name=$1
 
-if [ -z "${_repo_tag}" ]; then
-  source GRAFANA_META
-  _repo_tag=$REPO_TAG
-  _grafana_stamp=$GRAFANA_STAMP
-  _docker_tag=${REPO_TAG%-testing}
-  _grafana_version=${_docker_tag#v}
-  if [[ ${_grafana_version} == *beta* ]]; then
-    _pkg_name=${_grafana_version/-beta/-${GRAFANA_STAMP}beta}
-  else
-    _pkg_name=${_grafana_version}-${GRAFANA_STAMP}
+#set -x
+
+usage() {
+  base="$(basename "$0")"
+  cat <<EOUSAGE
+usage: $base [-d|-t|-p] [repo-tag] [stamp]
+Build docker images
+  -d show default
+  -t create & run docker container
+  -p push images to dockerhub
+EOUSAGE
+}
+
+setup() {
+  _repo_tag=$1
+  _grafana_stamp=$2
+
+  if [ -z "${_repo_tag}" ]; then
+    source GRAFANA_META
+    _repo_tag=$REPO_TAG
+    _grafana_stamp=$GRAFANA_STAMP
   fi
-fi
 
-echo "REPO_TAG : ${_repo_tag}"
-echo "GRAFANA PACKAGE NAME: ${_pkg_name}"
-echo "DOCKER TAG: ${_docker_tag}"
+  _docker_tag=${REPO_TAG%-testing}
+  _pkg_name=${_docker_tag#v}
+  if [ ! -z "${_grafana_stamp}" ]; then
+    if [[ ${_pkg_name} == *beta* ]]; then
+      _pkg_name=${_pkg_name/-beta/-${GRAFANA_STAMP}beta}
+    else
+      _pkg_name=${_pkg_name}-${GRAFANA_STAMP}
+    fi
+  fi
+}
 
-docker build                                   \
-  --pull                                       \
-  --build-arg REPO_TAG=${_repo_tag}            \
-  --build-arg PKG_NAME=${_pkg_name}            \
-  --tag "fg2it/grafana-armhf:${_docker_tag}" .
+build(){
+  echo "Building image for fg2it/grafana-armhf:${_docker_tag}"
+  docker build                                   \
+    --pull                                       \
+    --build-arg REPO_TAG=${_repo_tag}            \
+    --build-arg PKG_NAME=${_pkg_name}            \
+    --tag "fg2it/grafana-armhf:${_docker_tag}" .
+}
+
+test(){
+  echo "Running container fg2it/grafana-armhf:${_docker_tag}"
+  docker run -ti --rm --name=testgrafana${_docker_tag} -p 9999:3000 fg2it/grafana-armhf:${_docker_tag}
+}
+
+push(){
+  echo "Docker login:"
+  docker login
+  echo "Pushing fg2it/grafana-armhf:${_docker_tag}"
+  docker push fg2it/grafana-armhf:${_docker_tag}
+}
+
+MODE=build
+
+case "$1" in
+  -t)
+    MODE=test
+    shift
+    ;;
+  -p)
+    MODE=push
+    shift
+    ;;
+  -d)
+    MODE=default
+    shift
+    ;;
+  -*)
+    echo >&2 'error: unknown arg' "$1"
+    usage >&2
+    exit 1
+    ;;
+esac
+
+setup
+
+echo "REPO TAG             : ${_repo_tag}"
+echo "GRAFANA PACKAGE NAME : ${_pkg_name}"
+echo "DOCKER TAG           : ${_docker_tag}"
+
+case $MODE in
+  default)
+    exit
+    ;;
+  push)
+    push
+    ;;
+  test)
+    test
+    ;;
+  build)
+    build
+esac
