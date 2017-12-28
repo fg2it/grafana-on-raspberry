9b46ad62c6033d2e0e383e7c6d542d1ea8818b0a
fg2it <fgpg2it@gmail.com>
2017-12-28 11:27:28 +0000

    gosu for plugin install
    cf https://github.com/grafana/grafana-docker/commit/27f06d41fd9307f8d01845cf481bb7307614abf7

--- docker/v4.x/run.sh
+++ docker/v4.x/run.sh
@@ -35,7 +35,7 @@ if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   OLDIFS=$IFS
   IFS=','
   for plugin in ${GF_INSTALL_PLUGINS}; do
-    grafana-cli --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
+    gosu grafana grafana-cli --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
   done
   IFS=$OLDIFS
 fi
