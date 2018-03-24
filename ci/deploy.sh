4f57d8ce56f8c86e8c7167bb0cfd6097152bbdf0
fg2it <fgpg2it@gmail.com>
2018-03-24 20:10:26 +0000

    travis: painful yml ...

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-curl -T $DEB \
-     -H "X-Bintray-Debian-Distribution:wheezy,jessie,stretch" \
-     -H "X-Bintray-Debian-Component:main" \
-     -H "X-Bintray-Debian-Architecture:${ARCH}" \
-     -H "X-Bintray-Publish:1" \
-     -ufg2it:${BINTRAY_TOKEN} \
-     https://api.bintray.com/content/fg2it/${REPO}/grafana-on-raspberry/${GRAFANA_VERSION}/${COMPONENT}/g/$DEB
+
+if [ ! -z "$TRAVIS_TAG" ]; then
+  curl -T $DEB \
+       -H "X-Bintray-Debian-Distribution:wheezy,jessie,stretch" \
+       -H "X-Bintray-Debian-Component:main" \
+       -H "X-Bintray-Debian-Architecture:${ARCH}" \
+       -H "X-Bintray-Publish:1" \
+       -ufg2it:${BINTRAY_TOKEN} \
+       https://api.bintray.com/content/fg2it/${REPO}/grafana-on-raspberry/${GRAFANA_VERSION}/${COMPONENT}/g/$DEB
+fi
